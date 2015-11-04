28d26557d003ca766f388fa01c13d1042cb01f17
alu0100706734 <alu0100706734@ull.edu.es>
2015-11-04 21:57:08 +0000

    wworker funcionando

--- js/temperature.js
+++ js/temperature.js
@@ -6,72 +6,6 @@ function Medida(valor, tipo){
 	
 }
 
-function Temperatura(){
-    Medida.call(this); 
-}
-
-Temperatura.prototype = new Medida();
-Temperatura.prototype.ctof = function(){
-	var resultado = ((this.getValor() * 9/5)+32).toFixed(2);
-	return resultado;
-}
-
-
-Temperatura.prototype.ctok = function(){
-	var resultado = (this.getValor() + 273.15).toFixed(2);
-	return resultado;	
-}
-
-Temperatura.prototype.ftoc = function(){
-	var resultado = ((this.getValor * 5/9)-32).toFixed(2);
-	return resultado;
-}
-
-Temperatura.prototype.ftok = function(){
-	var resultado = ((this.getValor*5/9) + 459.67).toFixed(2);
-	return resultado;
-}
-
-Temperatura.prototype.ktoc = function(){
-	var resultado = (this.getValor - 273.15).toFixed(2);
-	return resultado;
-}
-
-Temperatura.prototype.ktof = function(){
-	var resultado = ((this.getValor() - 459.67) * 9/5).toFixed(2);
-	return resultado;
-}
-
-/*function Celsius(valor){
-	this.tipo = 'c';
-	Temperatura.call(this, valor,this.tipo);
-	this.Transform = function (){
-		var dato = parseInt(this.valor);
-		return (((this.valor * 9/5)+32).toFixed(2) + "º Farenheit " + (273.15).toFixed(2) + "º Kelvin");
-	}
-}*/
-
-/*function Farenheit(valor){
-	this.tipo = 'f';
-	Temperatura.call(this, valor, this.tipo);
-	this.Transform = function(){
-		var dato = parseInt(this.valor);
-		return (((this.valor-32)*5/9).toFixed(2) + "º Celsius " + ((dato+459.67)*5/9).toFixed(2) + "º Kelvin");
-	}
-}
-*/
-/*function Kelvin(valor){
-	this.tipo = 'k';
-	Temperatura.call(this,valor,this.tipo);
-	this.Transform = function(){
-		var dato = this.valor;
-		return ((this.valor-273.15).toFixed(2) + "º Celsius " + ((dato-459.67)*9/5).toFixed(2) + "º Farenheit");
-	}
-}*/
-
-function Error(){
-		return ("ERROR! Prueba con algo como '52.8E4K' ");
-}
 
 Medida.prototype.setValor = function(valor){
 	this.valor = valor;
@@ -89,14 +23,53 @@ Medida.prototype.getTipo = function(){
 	return this.tipo;
 }
 
+function Temperatura(){
+    Medida.call(this); 
+}
+
+Temperatura.prototype = new Medida();
+
+Temperatura.prototype.ctof = function(){
+	var resultado = ((this.getValor() * 9/5)+32);
+	return resultado;
+}
+
+Temperatura.prototype.ctok = function(){
+	var resultado = this.getValor() + 273.15;
+	return resultado;	
+}
+
+Temperatura.prototype.ftoc = function(){
+	
+	var resultado = (this.getValor() - 32)*5/9;
+	return resultado;
+}
+
+Temperatura.prototype.ftok = function(){
+	var resultado = ((this.getValor() -32)*5/9 + 273.15);
+	return resultado;
+}
+
+Temperatura.prototype.ktoc = function(){
+	var resultado = (this.getValor() - 273.15);
+	return resultado;
+}
+
+Temperatura.prototype.ktof = function(){
+	var resultado = ((this.getValor() - 459.67) * 9/5);
+	return resultado;
+}
+
 function entrada(){
 	var valor = original.value;
 	var regexp = /(^[-+]?\d+(?:\.\d*)?)(?:[eE]?([-+]?\d+))?\s*([cCfFkK])/;
     var m = valor.match(regexp);
 	if (m){
+		m[1] = parseFloat(m[1]);
 	var convertir = new Temperatura();
 	convertir.setTipo(m[3].toLowerCase());
 	convertir.setValor(m[1]);
+	convertir.resolver();
 	}
 	else {
 		document.getElementById("converter");
@@ -109,27 +82,19 @@ function entrada(){
 Temperatura.prototype.resolver = function() {
   
   var calculo = 0;
-  
-  /*if (m) {
-    var num = m[1];
-		num = parseFloat(num);
-    var tipo = m[3];
-    	tipo = tipo.toLowerCase();
-    var e = m[2];
-		e = parseInt(e);*/
 
-  if (this.GetTipo() == 'c'){
-	 calculo = this.ctof() + " Farenheit " + this.ctok()+ " Kelvin ";
+  if (this.getTipo() == 'c'){
+	 calculo = this.ctof().toFixed(2) + " Farenheit " + this.ctok().toFixed(2) + " Kelvin ";
   }
-  if (this.GetTipo() == 'f'){
-	calculo = this.ftoc()+ " Celsius " + this.ftok() + " Kelvin ";
+  if (this.getTipo() == 'f'){
+	calculo = this.ftoc().toFixed(2) + " Celsius " + this.ftok().toFixed(2) + " Kelvin ";
   }
  
-  if (this.GetTipo() == 'k'){
-	calculo = this.ktoc()+ " Celsius " + this.ktof() + " Farenheit"
+  if (this.getTipo() == 'k'){
+	calculo = this.ktoc().toFixed(2) + " Celsius " + this.ktof().toFixed(2) + " Farenheit";
   }
 
-  else if (this.GetTipo() != 'k' | 'f' | 'c') {
+  else if (this.getTipo() != 'k' | 'f' | 'c') {
     	converter.innerHTML = Error();
   	}
 	
